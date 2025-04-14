5265a7a2e20ac9e724eac502c60389840e7964c5
Andy Kent <andy@binaryalchemy.net>
2025-04-14 16:39:46 +0100

    add temporary cache key extension point
    # Conflicts:
    #	Package.resolved
    #	Sources/Apollo/Execution/GraphQLExecutor.swift
    #	Sources/Apollo/Internal Utilities/InputValue+Evaluation.swift
    #	Sources/ApolloAPI/SchemaConfiguration.swift

--- Sources/Apollo/Execution/GraphQLExecutor.swift
+++ Sources/Apollo/Execution/GraphQLExecutor.swift
@@ -90,7 +90,7 @@ public class FieldExecutionInfo {
   
   func cacheKeyForField() throws -> String {
     guard let _cacheKeyForField else {
-      let cacheKey = try field.cacheKey(with: parentInfo.variables)
+      let cacheKey = try field.cacheKey(with: parentInfo.variables, metadata: parentInfo.schema)
       _cacheKeyForField = cacheKey
       return cacheKey
     }
@@ -110,8 +110,7 @@ public class FieldExecutionInfo {
     // If the object has it's own cache key, reset the cache path to the key,
     // rather than using the inherited cache path from the parent field.
     let cachePath: ResponsePath = {
-      if let cacheKey { return [cacheKey] }
-      else { return self.cachePath }
+      if let cacheKey { return [cacheKey] } else { return self.cachePath }
     }()
 
     let childExecutionInfo = ObjectExecutionInfo(
--- Sources/Apollo/Internal Utilities/InputValue+Evaluation.swift
+++ Sources/Apollo/Internal Utilities/InputValue+Evaluation.swift
@@ -16,7 +16,7 @@ func CacheKeyForField(named fieldName: String, arguments: JSONObject) -> String 
   return argumentsKey.isEmpty ? fieldName : "\(fieldName)(\(argumentsKey))"
 }
 
-fileprivate func orderIndependentKey(for object: JSONObject) -> String {
+private func orderIndependentKey(for object: JSONObject) -> String {
   return object.sorted { $0.key < $1.key }.map {
     let value = $0.value as? AnyHashable
     switch value {
@@ -40,10 +40,16 @@ fileprivate func orderIndependentKey(for object: JSONObject) -> String {
 
 @_spi(Execution)
 extension Selection.Field {
-  public func cacheKey(with variables: GraphQLOperation.Variables?) throws -> String {
+  public func cacheKey(
+    with variables: GraphQLOperation.Variables?,
+    metadata: any SchemaMetadata.Type
+  )
+    throws -> String
+  {
     if let arguments = arguments {
       let argumentValues = try InputValue.evaluate(arguments, with: variables)
-      return CacheKeyForField(named: name, arguments: argumentValues)
+      return metadata.configuration.cacheKeyForField(named: name, arguments: argumentValues)
+        ?? CacheKeyForField(named: name, arguments: argumentValues)
     } else {
       return name
     }
